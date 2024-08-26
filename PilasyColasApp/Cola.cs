a3f5459e41e6ea5f1938c61263438706f3eafec6
Jeremias Perez <114047@tecnicatura.frc.utn.edu.ar>
2024-08-26 19:38:01 -0300

    Cambios Realizados

---
+++ PilasyColasApp/Cola.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilasyColasApp
+{
+    public class Cola : IColeccion
+    {
+        List<object> elementos;
+        public Cola()
+        {
+            elementos = new List<object>();
+
+        }
+        public bool Añadir(object obj)
+        {
+            elementos.Add(obj);
+            return true;
+        }
+
+        public bool estaVacia()
+        {
+            if (elementos.Count == 0) 
+            { 
+                return true; 
+            } 
+            else 
+            { 
+                return false; 
+            }
+        }
+
+        public object Extraer()
+        {
+            elementos.Remove(0);
+            return true;
+        }
+
+        public object Primero()
+        {
+            int tamaño = elementos.Count();
+            return elementos[tamaño - 1];
+        }
+    }
+}
---
+++ PilasyColasApp/IColeccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilasyColasApp
+{
+    public interface IColeccion
+    {
+        bool estaVacia();
+        object Extraer();
+        object Primero();
+        bool Añadir(Object obj);
+    }
+}
